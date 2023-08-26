37f78691ab4bac8ca7c1732a5fbed37240675bae
Anjali Sharma <107831444+Anjali-sharma1313@users.noreply.github.com>
2023-08-26 19:25:12 +0530

    Update and rename Wolverine.sol to Bronco.sol

--- Bronco.sol
+++ Bronco.sol
@@ -4,21 +4,18 @@ pragma solidity ^0.8.9;
 
 import "erc721a/contracts/ERC721A.sol";
 
-contract Wolverine is ERC721A{
+contract Bronco is ERC721A{
 
     address public owner;
 
     // Maximum number of tokens that can be minted
     uint256 public maxQuantity = 5;
 
-    // Base url for the nfts
-    string baseUrl = "https://gateway.pinata.cloud/ipfs/QmWkzzhkgjMAmFi86TGdEmPtjnomCpA1RndF2si6pvrmYy/";
-
     // URL for the prompt description
     string public prompt =
-        "Airbrush portrait of wolverine from xmen, by dimitri patelis marvel masterpieces , romero brito";
+        "a potrait of princess";
 
-    constructor() ERC721A("Wolverine", "WOL") {
+    constructor() ERC721A("Bronco", "BRC") {
         owner = msg.sender;
     }
 
@@ -34,10 +31,6 @@ contract Wolverine is ERC721A{
         _mint(msg.sender, quantity);
     }
 
-    // Override the baseURI function to return the base URL for the NFTs
-    function _baseURI() internal view override returns (string memory){
-        return baseUrl;
-    }
 
     // Return the URL for the prompt description
     function promptDescription() external view returns (string memory) {
