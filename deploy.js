ecf2a9eb64bd270368c6dce3ccf85d2dafd83b7a
Anjali Sharma <107831444+Anjali-sharma1313@users.noreply.github.com>
2023-08-26 19:28:11 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function main() {
   // Get the contract factory
-  const NFT = await hre.ethers.getContractFactory("Wolverine");
+  const NFT = await hre.ethers.getContractFactory("Bronco");
 
   // Deploy the contract
   const nft = await NFT.deploy();
